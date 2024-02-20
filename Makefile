f228c33e82c201a30ed84fe00e6ff5764033be63
Naichen Zhao <nzhao11235@berkeley.edu>
2024-02-19 16:17:10 -0800

    updated gitignore

--- Makefile
+++ Makefile
@@ -37,3 +37,12 @@ build_test:
 	-rm -fR src-gen/Main/build
 	cd src-gen/Main && cmake . -Bbuild
 	cd src-gen/Main/build && make
+
+#######################################
+# flash the controller
+#######################################
+just_flash:
+	openocd -f /Users/naichenzhao/openocd-0.12.0/tcl/interface/stlink.cfg -f /Users/naichenzhao/openocd-0.12.0/tcl/target/stm32f4x.cfg -c "program $(BUILD_DIR)/$(TARGET).elf verify reset exit"
+
+flash: build_dev
+	openocd -f /Users/naichenzhao/openocd-0.12.0/tcl/interface/stlink.cfg -f /Users/naichenzhao/openocd-0.12.0/tcl/target/stm32f4x.cfg -c "program $(BUILD_DIR)/$(TARGET).elf verify reset exit"
--- STM_sdk/Core/Src/stm32_startup.c
+++ STM_sdk/Core/Src/stm32_startup.c
@@ -6,37 +6,39 @@ UART_HandleTypeDef huart2;
 
 static uint8_t UART2_rxBuffer[12] = {0};
 
-int _write(int file, char *ptr, int len) {
+int _write(int file, char *ptr, int len)
+{
   HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, HAL_MAX_DELAY);
   return len;
 }
 
-
-static void MX_USART2_UART_Init(void) {
+static void MX_USART2_UART_Init(void)
+{
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
+  huart2.Init.BaudRate = 230400; // 115200 is base baud rate but we want to push this faster
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
   huart2.Init.Mode = UART_MODE_TX_RX;
   huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK) {
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
     __disable_irq();
-    while (1);
+    while (1)
+      ;
   }
 }
 
-int stm32_startup(void) {
+int stm32_startup(void)
+{
   MX_USART2_UART_Init();
   printf("--------- STM32 START------------ \r\n");
 
   main();
 }
 
-
-
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 
 void assert_failed(uint8_t *file, uint32_t line)
 {
@@ -45,4 +47,4 @@ void assert_failed(uint8_t *file, uint32_t line)
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
-#endif 
+#endif
